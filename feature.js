79036d333abd9bbcc1e4d26899a415318d901ae3
Islam4049 <70667310+Islam4049@users.noreply.github.com>
2020-10-27 19:08:38 -0400

    making some change on html and JS files

--- feature.js
+++ feature.js
@@ -1,24 +1,9 @@
-document.ready, function(){
-    var queryUrl ="https://api.nasa.gov/planetary/apod?api_key="+ apiKey;
-    var apiKey ="gYo6aP2v2kURjJFrRdzO4eOvx79sGG3ngPwy2USO";
-
-}
-$ajax({
-      url: queryUrl,   
-      method: "GET"
-}).then (function(response){
-    console.log(response)
+$(document).ready(function() {
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod?api_key=gYo6aP2v2kURjJFrRdzO4eOvx79sGG3ngPwy2USO"
+    }).then(function(data) {
+        console.log(data.hdurl);
+        $('#img').append('<img src="' + data.hdurl + '">');
+        console.log(data.hdurl);
+    });
 });
-
-
-
-
-req.addEventListener("load", function(){
-	if(req.status == 100 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-    document.getElementById("title").textContent = response.title;
-    document.getElementById("date").textContent = response.date;
-    document.getElementById("pic").src = response.hdurl;
-    document.getElementById("explanation").textContent = response.explanation;
-  }
-})
